9c811795b5126acf77a86817e8832eddbad98cf0
helf4ch <kirill24579@gmail.com>
2022-03-21 21:26:23 +1000

    added first project

---
+++ window-and-a-button/img_frame.py
@@ -0,0 +1,45 @@
+from tkinter import Frame, Canvas
+from PIL import Image, ImageTk
+
+class Image_frame(Frame):
+
+    def __init__(self, parent, img_path1, img_path2):
+        super().__init__(parent)
+        self.parent = parent
+       
+        self.img_path1 = img_path1
+        self.img_path2 = img_path2
+
+        self.open_img(self.img_path1)
+
+    def open_img(self, path):
+        self.img = Image.open(path)
+        self.photo = ImageTk.PhotoImage(self.img)
+        self.canvas = Canvas(self, height=self.img.size[1]+20, width=self.img.size[0]+20)
+        self.canvas.create_image(10, 10, anchor='nw', image=self.photo)
+        self.canvas.grid(row=0, column=0)
+        self.cur_path = path
+
+    def change_img(self):
+        if self.cur_path == self.img_path1:
+            self.cur_path = self.img_path2
+        else:
+            self.cur_path = self.img_path1
+
+        self.open_img(self.cur_path)
+
+
+def main():
+    # Test
+    from tkinter import Tk
+    window = Tk()
+    img_fr = Image_frame(window, "E:/Desktop/all/com-sci/python/WebProject1/img1.png", "img2.png")
+    window.columnconfigure(0, weight=1)
+    window.rowconfigure(0, weight=1)
+    img_fr.grid(row=0, column=0)
+    #img_fr.change_img()
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
+
---
+++ window-and-a-button/setup_window.py
@@ -0,0 +1,65 @@
+from tkinter import Tk, Button, Frame, messagebox
+from tkinter.filedialog import askopenfilename
+
+class Open_file_button(Button):
+    
+    def __init__(self, parent, text):
+        super().__init__(parent, text=text, command=self.open_file)
+        self.filepath = None
+
+    def open_file(self):
+        self.filepath = askopenfilename()
+
+        if not self.filepath:
+            return
+
+        self["text"] = f"{self.filepath}"
+
+class Setup_window(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.title("Setup")
+
+    def initUI(self):
+        self.columnconfigure(0, weight=1, minsize=400)
+        self.rowconfigure(0, weight=1, minsize=60)
+
+        self.btn_fr = Frame(self)
+
+        for i in range(0, 3):
+            self.btn_fr.rowconfigure(i, weight=1)
+
+        self.btn_fr.columnconfigure(0, weight=1)
+
+        self.btn_fr.grid(row=0, column=0, sticky='nsew')
+
+        self.top_btn = Open_file_button(self.btn_fr, 'First image')
+        self.mid_btn = Open_file_button(self.btn_fr, 'Second image')
+        self.btm_btn = Button(self.btn_fr, text='OK', command=self.close)
+
+        self.top_btn.grid(row=0, column=0, sticky='nsew', padx=5, pady=5)
+        self.mid_btn.grid(row=1, column=0, sticky='nsew', padx=5)
+        self.btm_btn.grid(row=2, column=0, sticky='nsew', padx=5, pady=5)
+
+    def close(self):
+        if not self.top_btn.filepath and not self.mid_btn.filepath:
+            messagebox.showerror("ERROR", "Images doesn't set")
+            return
+        elif not self.top_btn.filepath: 
+            messagebox.showerror("ERROR", "First image doesn't set")
+            return
+        elif not self.mid_btn.filepath:
+            messagebox.showerror("ERROR", "Second image doesn't set")
+            return
+
+        self.destroy()
+
+def main():
+    test = Setup_window()
+    test.mainloop()
+
+if __name__ == '__main__':
+    main()
+
