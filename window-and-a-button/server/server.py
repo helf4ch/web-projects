aa2be3c5dc9320b013a033beb9d3f697b9578c7a
helf4ch <kirill24579@gmail.com>
2022-03-30 22:37:46 +1000

    added server

--- window-and-a-button/server/server.py
+++ window-and-a-button/server/server.py
@@ -3,12 +3,76 @@ import asyncio
 import json
 
 
-IP = "185.242.106.198"
-PORT = 8080
+IP = "localhost" # args parser 
+PORT = 80
+
+
+connected = {}
+
+# connected:
+# {
+#       websocket:
+#           {
+#               "type": "ButtonApp" / "ImageApp"
+#           }
+# }
+
+
+# jsons:
+# requests:
+# {
+#       "from": "ButtonApp" / "ImageApp",
+#       "type": "connect",
+# }
+
+# {
+#       "from": "ButtonApp",
+#       "type": "changeImage",
+# }
+
+# answers:
+# {
+#       "type": "changeImage",
+# }
+
+
+async def onConnect(websocket, type):
+    connected[websocket]["type"] = type
+
+
+async def onChangeImage(websocket):
+    async for conn in connected[websocket]:
+        if conn["type"] == "ImageApp":
+            await conn.send(json.dumps( { "type": "changeImage" } ))
+
+
+async def messageHandler(websocket, message_parsed):
+    type = message_parsed["type"]
+    
+    if type == "connect":
+        await onConnect(websocket, message_parsed["from"])
+    
+    elif type == "changeImage":
+        await onChangeImage(websocket)
+    
+    else:
+        pass
+
+
+async def messageParse(message):
+    return json.loads(message)
 
 
 async def handler(websocket):
-    pass
+    print("A client just connected.")
+    try:
+        async for message in websocket:
+            await messageHandler(websocket, messageParse(message))
+    except websockets.exceptions.ConnectionClosed as e:
+        print("A client just disconnected.")
+    finally:
+        connected.remove(websocket)
+
 
 
 async def main():
